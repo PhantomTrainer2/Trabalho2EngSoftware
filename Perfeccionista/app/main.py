0e25d2791d69ede9a446115ea2726a16953f37de
Luiz Raffaini <eduardoraffaini@MacBook-Air-de-Eduardo.local>
2025-06-25 11:47:31 -0300

    a

--- Perfeccionista/app/main.py
+++ Perfeccionista/app/main.py
@@ -4,15 +4,18 @@ from infra.db.database import criar_banco_de_dados
 from app.routes.produto_routes import produto_bp
 from app.routes.fornecedor_routes import fornecedor_bp
 
+
 def create_app():
+
     app = Flask(__name__)
     CORS(app)
     app.register_blueprint(produto_bp)
     app.register_blueprint(fornecedor_bp)
-    
+
     criar_banco_de_dados()
-    
+
     return app
+
 
 if __name__ == '__main__':
     app = create_app()
